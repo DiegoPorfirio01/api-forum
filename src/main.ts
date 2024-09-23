cfa012ac61028bf3231f684420457283b9bb337a
Diego <porfiriodiego64@gmail.com>
2024-09-22 22:20:41 -0300

    feat(config module) implements validation enviroment variables

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { patchNestJsSwagger } from 'nestjs-zod';
+import { ConfigService } from '@nestjs/config';
+import { Env } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   patchNestJsSwagger();
+
+  const configService: ConfigService<Env, true> = app.get(ConfigService);
+  const port = configService.get('PORT', { infer: true });
 
   const config = new DocumentBuilder()
     .setTitle('API FORUM')
@@ -15,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3333);
+  await app.listen(port);
 }
 bootstrap();
